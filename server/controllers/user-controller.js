201140a2c1a6e638245b718d95c0dd8d3d610ebc
davieoba <bodunrindavidbond@gmail.com>
2023-07-25 06:24:09 +0100

    feat: add the goole-auth route

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,1 +1,20 @@
 const User = require('../models/user-model')
+const catchAsync = require('../utils/catchAsync')
+
+exports.register = catchAsync(async (req, res, next) => {
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  if (!user) throw new Error('Error creating user')
+
+  // send token
+})
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body
+
+  if (!email || !password) throw new Error('Email and Password required')
+})
--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,7 +1,11 @@
+const passport = require('passport')
 const router = require('express').Router()
 
+const singinWithgoogle = (req, res, next) => {
+  passport.authenticate('google', { scope: ['openid', 'profile', 'email'], session: false })(req, res, next)
+}
 
-router.get('/google', singinWithgoogle, (req, res) => {
+router.get('/google', singinWithgoogle, (req, res, next) => {
   // some code
 })
 
