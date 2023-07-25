cf9998d69ac617e8ca3c369c6c0f0443005fcf82
davieoba <bodunrindavidbond@gmail.com>
2023-07-25 08:43:01 +0100

    update passport callback url

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,12 +1,19 @@
 const passport = require('passport')
 const router = require('express').Router()
 
+router.get('/', (req, user) => { })
+
 const singinWithgoogle = (req, res, next) => {
   passport.authenticate('google', { scope: ['openid', 'profile', 'email'], session: false })(req, res, next)
 }
 
 router.get('/google', singinWithgoogle, (req, res, next) => {
-  // some code
+  // 
+})
+
+router.get('/google/redirect', (req, res, next) => {
+  res.cookie('token', 'some-cookie-data', { secure: false, httpOnly: false })
+  res.redirect('/')
 })
 
 module.exports = router
